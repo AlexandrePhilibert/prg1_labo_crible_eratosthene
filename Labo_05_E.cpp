8344e57cf419ebc460c298690daf12281a5325f2
Alexandre Philibert <alexandre.philibert@ik.me>
2022-11-15 18:17:13 +0100

    Initial commit

---
+++ Labo_05_E.cpp
@@ -0,0 +1,22 @@
+// -----------------------------------------------------------------------------------------------
+// Fichier        : Labo_05_E.cpp
+// Auteur(s)      : Sonnard Nicolas & Philibert Alexandre
+// Date           : 2022-11-15
+// But            :
+// Modifications  : NIL
+// Remarque(s)    :
+// Compilateur    : g++ 11.2.0
+// Standard C++   : C++ 20
+// -----------------------------------------------------------------------------------------------
+#include <iostream>
+#include <cstdlib>
+#include <limits>
+
+using namespace std;
+
+int main() {
+   cout << "Pressez ENTER pour quitter";
+   cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+   return EXIT_SUCCESS;
+}
